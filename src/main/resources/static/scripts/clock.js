2e985b2129669271848c26c911ff738955f4056a
Silvio-Pitti <team.asimov.sp@gmail.com>
2024-09-03 19:24:36 +0200

    Fix layout of timePunch page
    Took 4 hours 4 minutes

--- src/main/resources/static/scripts/clock.js
+++ src/main/resources/static/scripts/clock.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   clock();
   function clock() {
-    document.getElementById("date-time").innerHTML = new Date().toLocaleString("it-it");
+    document.getElementById("date-time").innerHTML = "Time: " + new Date().toLocaleString("it-it");
     document.getElementById("dateTime").value = document.getElementById("date-time").innerHTML;
     setTimeout(clock, 1000);
   }
