9565b2ed177b5a954a02677ff816abc272dba411
Silvio-Pitti <team.asimov.sp@gmail.com>
2024-09-09 11:29:57 +0200

    Configure for https
    Took 2 hours 46 minutes

---
+++ src/main/java/com/asimov/timeTracerSpringWeb/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.asimov.timeTracerSpringWeb.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
